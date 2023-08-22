f9f7feddf2516bc5b7180b7e0290acc3e40f0d94
USER <USER@LAPTOP-IPA4K501>
2023-08-22 11:58:01 +0530

    packages

---
+++ Java/src/main/java/acessmod2/Access.java
@@ -0,0 +1,16 @@
+package acessmod2;
+
+import acessmodifier.AccessmodifierTypes;
+
+public class Access {
+
+	public static void main(String[] args) {
+		AccessmodifierTypes a= new AccessmodifierTypes();
+		a.sum();
+	//	a.add();
+	//	a.print();
+	//	a.sub();
+
+	}
+
+}
---
+++ Java/src/main/java/acessmod2/AccessMod.java
@@ -0,0 +1,17 @@
+package acessmod2;
+
+import acessmodifier.AccessmodifierTypes;
+
+public class AccessMod {
+
+	
+		public static void main(String[] args) {
+			AccessmodifierTypes a= new AccessmodifierTypes();
+			a.sum();
+			a.add();
+			a.print();
+		a.sub();
+
+	}
+
+}
